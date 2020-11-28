f51cd85db2876a77dbda0dcc0fdee8d72cf17261
floffah <therealfloffah@gmail.com>
2020-11-28 22:39:56 +0000

    init configs

---
+++ snowpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mount: {
+        public: "/",
+        src: "/media"
+    },
+    exclude: ['**/node_modules/**/*', '**/__tests__/*', '**/*.@(spec|test).@(js|mjs)', '**/*.stories.@(tsx|mdx)'],
+    plugins: ["@snowpack/plugin-optimize", "@snowpack/plugin-typescript", "@snowpack/plugin-webpack"],
+    installOptions: {
+        treeshake: true,
+        installTypes: true,
+    },
+    buildOptions: {
+        sourceMaps: true,
+        baseUrl: "./",
+        out: "build",
+    }
+}
